5eefded59c382d98902ab3285f414ef9e45561fa
Ian McCormack <icmccorm@andrew.cmu.edu>
2024-04-21 10:43:35 -0400

    Updated jobs script with new increments.

--- scripts/eval/generate_jobs.sh
+++ scripts/eval/generate_jobs.sh
@@ -1,11 +1,15 @@
 
-for j in $(seq 0 $2 $1)
+POSITIONAL_INCREMENT=$1
+ARBITRARY_INCREMENT=$2
+for STEP in $(seq 0 1 50)
 do
-    for m in $(seq 1 1 4)
+    POSITIONAL_BUDGET=$(echo "$STEP * $POSITIONAL_INCREMENT" | bc)
+    ARBITRARY_BUDGET=$(echo "$STEP * $ARBITRARY_INCREMENT" | bc)
+    for PRIVACY_MODE in $(seq 1 1 4)
     do
-        for i in {1..5}
+        for CHANGE_SCHEME in {1..5}
         do
-            printf "%s\\n%s\\n%s\\n%s\\n" "$i" "$j" "$m" "1"
+            printf "%s\\n%s\\n%s\\n%s\\n" "$CHANGE_SCHEME" "$POSITIONAL_BUDGET" "$ARBITRARY_BUDGET" "$PRIVACY_MODE" "1"
         done
     done
 done
